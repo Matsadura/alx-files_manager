5ca56dbe99ba885457927a96750af9f56e30d66d
Matsadura <zidanditoo1@gmail.com>
2024-10-16 10:33:54 +0100

    [UPDATE] Fix error handling msg

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,13 +12,13 @@ class UsersController {
       return;
     }
     if (!password) {
-      res.status(400).json({ error: 'Mising password' });
+      res.status(400).json({ error: 'Missing password' });
       return;
     }
     const users = dbClient.usersCollection;
     users.findOne({ email }, (err, user) => {
       if (user) {
-        res.status(400).json({ error: 'Already exists' });
+        res.status(400).json({ error: 'Already exist' });
       } else {
         const hashedPassword = sha1(password);
         users.insertOne(
